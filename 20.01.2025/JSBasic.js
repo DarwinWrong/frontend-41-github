737781a283af828134d2cbae89106a0eb95df44d
Олена Поліщук <polishchuk_ol@donnu.edu.ua>
2025-01-22 21:45:06 +0200

    оголошення нових змінних

--- 20.01.2025/JSBasic.js
+++ 20.01.2025/JSBasic.js
@@ -2,4 +2,11 @@ var personName = 'Maria Petrova';
 let personEmail = 'mariapetrova@gmail.com';
 const personAge = '25';
 
-console.log('personName', personName,'personEmail', personEmail, 'personAge', personAge)
+console.log('personName', personName, 'personEmail', personEmail, 'personAge', personAge);
+
+personName = 'Anna Ivanova';
+personEmail = 'annaivanova@gmail.com';
+// personAge = '30';
+console.error('Помилка зміни віку');
+
+console.log('personName', personName, 'personEmail', personEmail, 'personAge', personAge);
