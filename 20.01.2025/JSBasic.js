9a406b6296ddda553a78f38db625a4ba1acb89cc
Олена Поліщук <polishchuk_ol@donnu.edu.ua>
2025-01-22 21:31:24 +0200

    оголошення нових 3 змінних

---
+++ 20.01.2025/JSBasic.js
@@ -0,0 +1,5 @@
+var personName = 'Maria Petrova';
+let personEmail = 'mariapetrova@gmail.com';
+const personAge = '25';
+
+console.log('personName', personName,'personEmail', personEmail, 'personAge', personAge)
