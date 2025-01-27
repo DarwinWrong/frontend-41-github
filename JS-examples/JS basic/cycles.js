475f5e65933d9271d579ef73727114bc566913cd
Bohdan Kozhevnik <kozhevnik_b@itstep.academy>
2025-01-27 14:50:46 +0200

    update

--- JS-examples/JS basic/cycles.js
+++ JS-examples/JS basic/cycles.js
@@ -51,3 +51,14 @@ for (let i = 0; i < 5; i++) {
     }
     console.log(i);
 }
+
+
+// =====================
+outerLoop: for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        if (i === 1 && j === 1) {
+            break outerLoop; // вихід з обох циклів
+        }
+        console.log(`i = ${i}, j = ${j}`);
+    }
+}
