7a6614cd354e986e4da48f143326a6774a8fe951
bertashevskyyv <bertashevskyyv@gmail.com>
2025-02-06 20:25:57 +0200

    Add JSDom second part of homework

---
+++ Homework(JS)/JS basic/JSDom_2/main.js
@@ -0,0 +1,18 @@
+// Change text color
+
+// const changeTextColor = () => {
+//     let text = document.getElementById("color");
+//     text.style.color = 'rgb(149,149,149)';
+// }
+
+//Modal
+
+const modalOpen = () => {
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'block';
+}
+
+const modalClose = () => {
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'none';
+}
