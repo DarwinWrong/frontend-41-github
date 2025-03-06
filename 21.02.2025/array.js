f696fa06139166aaf4baf2893f713ae8edc96ad0
Олена Поліщук <polishchuk_ol@donnu.edu.ua>
2025-03-06 20:11:07 +0200

    js basic

---
+++ 21.02.2025/array.js
@@ -0,0 +1,34 @@
+//2.1 task
+const names = ["John", "Sarah", "Michael", "Anna"];
+
+// 2.2 task
+
+function displayNames(arr) {
+    console.log(arr[1], arr[3])
+}
+displayNames(names);
+
+//2.3 task
+
+function showText(text) {
+    for (const name of text) {
+        console.log(`Персонаж ${name} " бере участь у розслідуванні.`)
+    }
+}
+showText(names)
+
+//2.4 task
+
+const person = {
+    name: 'Tom',
+    showName() {
+        console.log(this.name)
+    }
+}
+person.showName()
+
+//2.5 task
+
+const newPerson = Object.create(person)
+newPerson.surname = 'Antonov'
+console.log(newPerson);
---
+++ 21.02.2025/object.js
@@ -0,0 +1,44 @@
+// 1.1 task
+const characters = {
+    name: "Шерлок Холмс",
+    age: 40,
+    profession: "Детектив",
+    mysterySolved: 50
+};
+
+function displayCharacters(characters) {
+        console.log(`Імʼя: ${characters.name}, вік: ${characters.age}`)
+    ;
+}
+
+displayCharacters(characters)
+
+// 1.2 task
+
+delete characters.mysterySolved;
+console.log(characters)
+
+// 1.3 task
+
+function findObject(obj, prop) {
+        if (prop in obj) {
+          console.log(`${prop} властивість є`);
+        } else {
+          console.log("Даної властивості немає");
+        }
+}
+findObject(characters, 'age')
+findObject(characters, 'mysterySolved')
+ 
+//1.4 task
+
+const newCharacters = Object.create(characters)
+newCharacters.country = 'England'
+console.log(newCharacters)
+
+//1.5 task 
+for (const key in characters) {
+    console.log(` ${key} : ${characters[key]}`);
+}
+
+
